2b8bddd014f2555d861aa5ffaf12023fb5702ced
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-09-27 00:28:17 -0400

    Learned how to initialize vectors and access specific element

--- Main.R
+++ Main.R
@@ -1,7 +1,12 @@
 # Comments in R
 
 n <- floor(rnorm(10000, 500, 100)) #Generate 100000 numbers with a mean of 500 and a stdev of 100. Floored after.
-
 t <- table(n) #counts the frequency of each number in t
+barplot(table(floor(rnorm(10000, 500, 100))), xlab="Numbers", ylab="Frequencies") #Plots a bargraph with the said data and axis
 
-barplot(table(floor(rnorm(10000, 500, 100))), xlab="Numbers", ylab="Frequencies")
+print(class(n)) #Fetches the class the valiable belongs to
+
+#vector
+fruit <- c("apple", "pear", "banana") #vector with more than one entry
+print (fruit[1]) #Access element, arrays start at 1 :(
+print(class(fruit))
